6fddab450d0652d318740c6907883e9a134b3387
eRestin <lukasz.iljaszewicz@gmail.com>
2013-12-03 14:17:44 +0100

    debug for 404 changed

--- mhnweb/settings.py
+++ mhnweb/settings.py
@@ -111,7 +111,7 @@ LANGUAGE_CODE = "en"
 # are displayed for error pages. Should always be set to ``False`` in
 # production. Best set to ``True`` in local_settings.py
 DEBUG = True
-DEBUG404 = True
+DEBUG404 = False
 
 # Whether a user's session cookie expires when the Web browser is closed.
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
