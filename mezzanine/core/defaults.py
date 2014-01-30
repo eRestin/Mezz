578891cea6f7f06bdd9f3f621be5cc9f3a913bb2
eRestin <lukasz.iljaszewicz@gmail.com>
2014-01-30 11:46:54 +0100

    Message max-width

--- mezzanine/core/defaults.py
+++ mezzanine/core/defaults.py
@@ -129,8 +129,8 @@ register_setting(
 register_setting(
     name="FORMS_USE_HTML5",
     description=_("If ``True``, website forms will use HTML5 features."),
-    editable=True,
-    default=True,
+    editable=False,
+    default=False,
 )
 
 register_setting(
