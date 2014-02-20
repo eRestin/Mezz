b48587a405fd1091e91cbabd1a24eb6609fece04
eRestin <lukasz.iljaszewicz@gmail.com>
2014-02-20 13:21:51 +0100

    email settings added part2

--- mhnweb/settings.py
+++ mhnweb/settings.py
@@ -153,6 +153,7 @@ STATICFILES_FINDERS = (
 CONTACT_EMAIL = 'email4django@gmail.com'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'email4django@gmail.com'
 EMAIL_HOST_PASSWORD = 'trudnehaslo'
